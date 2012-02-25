d6b6a095b6d01b54a91ed5740c01f0052bf5b0f8
Raimond Spekking <raymond@users.mediawiki.org>
2012-02-25 20:36:21 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- AdminLinks.i18n.php
+++ AdminLinks.i18n.php
@@ -1824,11 +1824,12 @@ $messages['yi'] = array(
 
 /** Simplified Chinese (‪中文(简体)‬)
  * @author Franklsf95
+ * @author Xiaomingyan
  */
 $messages['zh-hans'] = array(
 	'adminlinks' => '管理员链接',
 	'adminlinks-desc' => '添加一个[[Special:AdminLinks|特殊页面]]为管理员提供有用的链接',
-	'adminlinks_documentation' => '$1 文档',
+	'adminlinks_documentation' => '$1说明文件',
 	'adminlinks_general' => '一般',
 	'adminlinks_editsidebar' => '编辑侧边栏',
 	'adminlinks_editcss' => '编辑 CSS 文件',
