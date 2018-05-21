3a762f03a0db3f2e21deefb74228b702f3d21e4c
Yaron Koren <yaron57@gmail.com>
2018-05-21 17:40:21 +0000

    Simplified AdminLinks.php formatting
    Change-Id: Ie00f8e7f06b838a1928faa9c3e699716875f2e79

--- AdminLinks.php
+++ AdminLinks.php
@@ -5,31 +5,31 @@
  * @author Yaron Koren
  */
 
-if ( defined( 'MEDIAWIKI' ) ) {
-	// credits
-	$GLOBALS['wgExtensionCredits']['specialpage'][] = array(
-		'path' => __FILE__,
-		'name' => 'Admin Links',
-		'version' => '0.2.3',
-		'author' => 'Yaron Koren',
-		'url' => 'https://www.mediawiki.org/wiki/Extension:Admin_Links',
-		'descriptionmsg' => 'adminlinks-desc',
-		'license-name' => 'GPL-2.0-or-later'
-	);
+if ( !defined( 'MEDIAWIKI' ) ) die;
 
-	$GLOBALS['wgAdminLinksIP'] = __DIR__ . '/';
-	$GLOBALS['wgMessagesDirs']['AdminLinks'] = __DIR__ . '/i18n';
-	$GLOBALS['wgExtensionMessagesFiles']['AdminLinksAlias'] =
-		$GLOBALS['wgAdminLinksIP'] . 'AdminLinks.alias.php';
-	$GLOBALS['wgSpecialPages']['AdminLinks'] = 'AdminLinks';
-	$GLOBALS['wgHooks']['PersonalUrls'][] = 'AdminLinks::addURLToUserLinks';
-	$GLOBALS['wgAvailableRights'][] = 'adminlinks';
-	// by default, sysops see the link to this page
-	$GLOBALS['wgGroupPermissions']['sysop']['adminlinks'] = true;
-	$GLOBALS['wgAutoloadClasses']['AdminLinks']
-		= $GLOBALS['wgAutoloadClasses']['ALTree']
-		= $GLOBALS['wgAutoloadClasses']['ALSection']
-		= $GLOBALS['wgAutoloadClasses']['ALRow']
-		= $GLOBALS['wgAutoloadClasses']['ALItem']
-		= $GLOBALS['wgAdminLinksIP'] . 'AdminLinks_body.php';
-}
+// credits
+$GLOBALS['wgExtensionCredits']['specialpage'][] = array(
+	'path' => __FILE__,
+	'name' => 'Admin Links',
+	'version' => '0.2.3',
+	'author' => 'Yaron Koren',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:Admin_Links',
+	'descriptionmsg' => 'adminlinks-desc',
+	'license-name' => 'GPL-2.0-or-later'
+);
+
+$GLOBALS['wgAdminLinksIP'] = __DIR__ . '/';
+$GLOBALS['wgMessagesDirs']['AdminLinks'] = __DIR__ . '/i18n';
+$GLOBALS['wgExtensionMessagesFiles']['AdminLinksAlias'] =
+	$GLOBALS['wgAdminLinksIP'] . 'AdminLinks.alias.php';
+$GLOBALS['wgSpecialPages']['AdminLinks'] = 'AdminLinks';
+$GLOBALS['wgHooks']['PersonalUrls'][] = 'AdminLinks::addURLToUserLinks';
+$GLOBALS['wgAvailableRights'][] = 'adminlinks';
+// by default, sysops see the link to this page
+$GLOBALS['wgGroupPermissions']['sysop']['adminlinks'] = true;
+$GLOBALS['wgAutoloadClasses']['AdminLinks']
+	= $GLOBALS['wgAutoloadClasses']['ALTree']
+	= $GLOBALS['wgAutoloadClasses']['ALSection']
+	= $GLOBALS['wgAutoloadClasses']['ALRow']
+	= $GLOBALS['wgAutoloadClasses']['ALItem']
+	= $GLOBALS['wgAdminLinksIP'] . 'AdminLinks_body.php';
