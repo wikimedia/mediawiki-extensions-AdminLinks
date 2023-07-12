5f258a4c2ec8bf13d0c9b8fdface1d414bbde1a9
Umherirrender <umherirrender_de.wp@web.de>
2023-07-12 20:13:55 +0200

    Convert hook to HookHandler
    Bug: T338453
    Change-Id: Iafa88e13e5d691710689fe8256df6c98db84e541

--- includes/AdminLinks.php
+++ includes/AdminLinks.php
@@ -79,55 +79,6 @@ class AdminLinks extends SpecialPage {
 		$out->addHTML( $admin_links_tree->toString() );
 	}
 
-	/**
-	 * For administrators, add a link to the special 'AdminLinks' page
-	 * among the user's "personal URLs" at the top, if they have
-	 * the 'adminlinks' permission.
-	 *
-	 * @param SkinTemplate $skinTemplate
-	 * @param array &$links
-	 */
-	public static function addURLToUserLinks(
-		$skinTemplate,
-		&$links
-	): void {
-		if ( !$skinTemplate->getUser()->isAllowed( 'adminlinks' ) ) {
-			return;
-		}
-
-		// If skin lacks the "user-menu" key, just exit.
-		// @todo Add support, if possible, for skins that lack
-		// this key, like Chameleon.
-		if ( !array_key_exists( 'user-menu', $links ) ) {
-			return;
-		}
-
-		$al = SpecialPage::getTitleFor( 'AdminLinks' );
-		$href = $al->getLocalURL();
-		$adminLinksVals = [
-			'text' => $skinTemplate->msg( 'adminlinks' )->text(),
-			'href' => $href,
-			'active' => ( $href == $skinTemplate->getTitle()->getLocalURL() )
-		];
-
-		// Find the location of the 'my preferences' link, and
-		// add the link to 'AdminLinks' right before it.
-		// This is a "key-safe" splice - it preserves both the
-		// keys and the values of the array, by editing them
-		// separately and then rebuilding the array.
-		// Based on the example at http://us2.php.net/manual/en/function.array-splice.php#31234
-		$menuKeys = array_keys( $links['user-menu'] );
-		$menuValues = array_values( $links['user-menu'] );
-		$prefsLocation = array_search( 'preferences', $menuKeys );
-		array_splice( $menuKeys, $prefsLocation, 0, 'adminlinks' );
-		array_splice( $menuValues, $prefsLocation, 0, [ $adminLinksVals ] );
-
-		$links['user-menu'] = [];
-		for ( $i = 0; $i < count( $menuKeys ); $i++ ) {
-			$links['user-menu'][$menuKeys[$i]] = $menuValues[$i];
-		}
-	}
-
 	/** @inheritDoc */
 	protected function getGroupName() {
 		return 'users';
---
+++ includes/AdminLinksHooks.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Hook handler class
+ */
+
+// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+
+class AdminLinksHooks implements
+	\MediaWiki\Hook\SkinTemplateNavigation__UniversalHook
+{
+
+	/**
+	 * For administrators, add a link to the special 'AdminLinks' page
+	 * among the user's "personal URLs" at the top, if they have
+	 * the 'adminlinks' permission.
+	 *
+	 * @param SkinTemplate $skinTemplate
+	 * @param array &$links
+	 */
+	public function onSkinTemplateNavigation__Universal( $skinTemplate, &$links ): void {
+		if ( !$skinTemplate->getUser()->isAllowed( 'adminlinks' ) ) {
+			return;
+		}
+		// If skin lacks the "user-menu" key, just exit.
+		// @todo Add support, if possible, for skins that lack
+		// this key, like Chameleon.
+		if ( !array_key_exists( 'user-menu', $links ) ) {
+			return;
+		}
+
+		$al = SpecialPage::getTitleFor( 'AdminLinks' );
+		$href = $al->getLocalURL();
+		$adminLinksVals = [
+			'text' => $skinTemplate->msg( 'adminlinks' )->text(),
+			'href' => $href,
+			'active' => ( $href == $skinTemplate->getTitle()->getLocalURL() )
+		];
+
+		// Find the location of the 'my preferences' link, and
+		// add the link to 'AdminLinks' right before it.
+		// This is a "key-safe" splice - it preserves both the
+		// keys and the values of the array, by editing them
+		// separately and then rebuilding the array.
+		// Based on the example at http://us2.php.net/manual/en/function.array-splice.php#31234
+		$menuKeys = array_keys( $links['user-menu'] );
+		$menuValues = array_values( $links['user-menu'] );
+		$prefsLocation = array_search( 'preferences', $menuKeys );
+		array_splice( $menuKeys, $prefsLocation, 0, 'adminlinks' );
+		array_splice( $menuValues, $prefsLocation, 0, [ $adminLinksVals ] );
+
+		$links['user-menu'] = [];
+		for ( $i = 0; $i < count( $menuKeys ); $i++ ) {
+			$links['user-menu'][$menuKeys[$i]] = $menuValues[$i];
+		}
+	}
+
+}
